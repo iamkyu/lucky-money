6ddd17c6cb746b90a9c6ab683d700d93594a06c7
남규진 <kyujnam@gmail.com>
2020-06-25 20:52:25 +0900

    init

---
+++ src/test/java/com/namkyujin/lucky/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.namkyujin.lucky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void shouldContextLoads() {
+    }
+}
