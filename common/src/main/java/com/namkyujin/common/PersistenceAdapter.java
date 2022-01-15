ae49540863c1d9bf17cc475f9123fc8d28523929
iamkyu <kyujnam@gmail.com>
2022-01-15 18:09:49 +0900

    feat: common 모듈 추가

---
+++ common/src/main/java/com/namkyujin/common/PersistenceAdapter.java
@@ -0,0 +1,25 @@
+package com.namkyujin.common;
+
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface PersistenceAdapter {
+
+    /**
+     * 영속성 어댑터임을 드러내기 위한 마커 인터페이스
+     */
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+
+}
